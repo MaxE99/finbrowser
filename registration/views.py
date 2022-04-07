2149a081106e0488cca1fb3fbf0ed199d46b85c1
MaxE99 <me-99@live.de>
2022-04-07 08:27:03 +0200

    added login and logout

--- registration/views.py
+++ registration/views.py
@@ -1,32 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
-from accounts.forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
+from accounts.forms import UserCreationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_creation_form = UserCreationForm(request.POST)
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+            return redirect('../../home/browser/')
+    else:
+        user_creation_form = UserCreationForm()
+    context = {'user_creation_form': user_creation_form}
+    return render(request, 'registration/register.html', context)
 
 
 def login_view(request):
     if request.method == "POST":
-        print("POST METHOD")
-        user_login_form = AuthenticationForm(request.POST)
+        user_login_form = AuthenticationForm(data=request.POST)
         if user_login_form.is_valid():
-            print("IS VALID")
-            user = user_login_form.get_user()
-            login(request, user)
-            return redirect('../../home/browser/')
+            cleaned_username = user_login_form.cleaned_data["username"]
+            cleaned_password = user_login_form.cleaned_data["password"]
+            user = authenticate(username=cleaned_username,
+                                password=cleaned_password)
+            if user:
+                login(request, user)
+                return redirect('../../home/browser/')
+            else:
+                pass  # user failed auth, send message back to user
+        else:
+            pass  # form not valid, handle errors
     else:
-        print("GET METHOD")
         user_login_form = AuthenticationForm()
     context = {'user_login_form': user_login_form}
     return render(request, 'registration/login.html', context)
 
 
-def register(request):
-    if request.method == 'POST':
-        user_creation_form = RegisterForm(request.POST)
-        if user_creation_form.is_valid():
-            user_creation_form.save()
-            return redirect('../../home/browser/')
-    else:
-        user_creation_form = RegisterForm()
-    context = {'user_creation_form': user_creation_form}
-    return render(request, 'registration/register.html', context)
+def logout_view(request):
+    # if request.method == "POST":
+    logout(request)
+    return redirect('../../registration/login/')
