db3c0a4af25ab37201ee07327dc3edce5273f6e5
MaxE99 <me-99@live.de>
2022-04-10 22:36:12 +0200

    refactored list_details + main page

--- home/api.py
+++ home/api.py
@@ -7,43 +7,8 @@ from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 # Local imports
-from home.models import Article, BrowserSource, BrowserCategory, Source, List
+from home.models import Article, Source, List
 from home.serializers import List_Serializer, Article_Serializer, Source_Serializer
-
-
-@api_view(["DELETE"])
-def category_delete(request, category):
-    category = get_object_or_404(BrowserCategory, name=category)
-    deleted_category = str(category)
-    category.delete()
-    return Response(f'"{deleted_category}" has been deleted')
-
-
-@api_view(["DELETE"])
-def source_delete(request, source):
-    source = get_object_or_404(Source, domain=source)
-    browser_source = get_object_or_404(BrowserSource, source=source)
-    deleted_source = str(source)
-    browser_source.delete()
-    return Response(f'"{deleted_source}" has been deleted')
-
-
-@api_view(["POST"])
-def category_add(request, category):
-    new_category = BrowserCategory(name=category)
-    new_category.save()
-    return Response(f'"{str(new_category)}" has been saved')
-
-
-@api_view(["POST"])
-def category_change(request, source, new_category):
-    source = get_object_or_404(Source, domain=source)
-    browser_source = get_object_or_404(BrowserSource, source=source)
-    category = get_object_or_404(BrowserCategory, name=new_category)
-    browser_source.category = category
-    browser_source.save()
-    return Response(
-        f'"{str(source)}" has been added to category {new_category}')
 
 
 @api_view(['POST'])
--- home/forms.py
+++ home/forms.py
@@ -2,14 +2,7 @@
 from django import forms
 from django.forms import ModelForm
 # Local imports
-from home.models import BrowserSource, List
-
-
-class AddSourceForm(ModelForm):
-
-    class Meta:
-        model = BrowserSource
-        fields = ['source', 'category']
+from home.models import List
 
 
 class AddListForm(ModelForm):
@@ -18,23 +11,3 @@ class AddListForm(ModelForm):
         model = List
         fields = ['name', 'content_type', 'sources']
         widgets = {'sources': forms.CheckboxSelectMultiple}
-
-
-# class SearchSettingsForm(forms.Form):
-#     SEARCH_TIMEFRAME_CHOICES = [
-#         ('All', 'All'),
-#         ('Last 24 Hours', 'Last 24 Hours'),
-#         ('Last 7 Days', 'Last 7 Days'),
-#         ('Last 30 Days', 'Last 30 Days'),
-#         ('Last 365 Days', 'Last 365 Days'),
-#     ]
-#     sources = forms.ModelMultipleChoiceField(
-#         queryset=BrowserSource.objects.all().order_by('domain'),
-#         initial=[option for option in BrowserSource.objects.all()],
-#         widget=forms.CheckboxSelectMultiple)
-#     timeframe = forms.ChoiceField(choices=SEARCH_TIMEFRAME_CHOICES)
-#     search_term = forms.CharField(
-#         required=False,
-#         max_length=120,
-#         widget=forms.TextInput(
-#             attrs={'placeholder': 'Enter specific search term'}))
