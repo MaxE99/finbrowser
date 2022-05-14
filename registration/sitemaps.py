2735d88880a54e936cd9dc67330c48824be71c60
MaxE99 <me-99@live.de>
2022-05-14 09:35:54 +0200

    refactored django-allauth registration

--- registration/sitemaps.py
+++ registration/sitemaps.py
@@ -2,15 +2,13 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-
 class RegistrationSitemaps(Sitemap):
-
     changefreq = "monthly"
     priority = 0.4
     protocol = 'https'
 
     def items(self):
-        return ['registration:login', 'registration:register']
+        return ['account_login', 'account_signup']
 
     def location(self, item):
         return reverse(item)
