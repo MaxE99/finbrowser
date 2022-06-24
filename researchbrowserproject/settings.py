70e44bbdf4d637784da3ed01c2da62b93b3ce85a
MaxE99 <me-99@live.de>
2022-06-24 12:54:56 +0200

    added procfile

--- researchbrowserproject/settings.py
+++ researchbrowserproject/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'researchbrowser.herokuapp.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
