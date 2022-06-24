a23162868cb5e6b0d2b1401d65ae2e8d87c5146a
MaxE99 <me-99@live.de>
2022-06-24 12:13:07 +0200

    commented staticfiles_dirs out

--- researchbrowserproject/settings.py
+++ researchbrowserproject/settings.py
@@ -20,10 +20,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # os.path.join(BASE_DIR, 'home/static/home/favicons')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     # os.path.join(BASE_DIR, 'home/static/home/favicons')
+# ]
 
 
 # Quick-start development settings - unsuitable for production
