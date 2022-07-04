80da97c9e2cf75d2510604322bce67cf623adb20
MaxE99 <me-99@live.de>
2022-07-04 15:46:00 +0200

    added admin registration for new models

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -1,7 +1,12 @@
 # Django Imports
 from django.contrib import admin
 # Local imports
-from apps.article.models import Article, HighlightedArticle
+from apps.article.models import Article, HighlightedArticle, AudioOfTheWeek, ArticleOfTheWeek, TrendingTopicArticle, EnergyCrisisTweet, MacroTweets
 
 admin.site.register(HighlightedArticle)
 admin.site.register(Article)
+admin.site.register(AudioOfTheWeek)
+admin.site.register(ArticleOfTheWeek)
+admin.site.register(TrendingTopicArticle)
+admin.site.register(EnergyCrisisTweet)
+admin.site.register(MacroTweets)
