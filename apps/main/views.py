c71213244bb7734bdfa5563025d865e694d013b9
MaxE99 <me-99@live.de>
2022-07-05 20:51:54 +0200

    fixed another bug at new home page

--- apps/main/views.py
+++ apps/main/views.py
@@ -11,11 +11,11 @@ class MainView(TemplateView, BaseMixin):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['articles_of_the_week'] = ArticleOfTheWeek.objects.all()
-        context['energy_crisis_tweets'] = EnergyCrisisTweet.objects.all()
-        context['macro_tweets'] = MacroTweets.objects.all()
-        context['trending_topic_articles'] = TrendingTopicArticle.objects.all()
+        context['articles_of_the_week'] = ArticleOfTheWeek.objects.all().select_related('article__source', 'article__source__sector', 'article__source__website').order_by('-article__pub_date')
+        context['energy_crisis_tweets'] = EnergyCrisisTweet.objects.all().select_related('article__source', 'article__source__sector', 'article__source__website').order_by('-article__pub_date')
+        context['macro_tweets'] = MacroTweets.objects.all().select_related('article__source', 'article__source__sector', 'article__source__website').order_by('-article__pub_date')
+        context['trending_topic_articles'] = TrendingTopicArticle.objects.all().select_related('article__source', 'article__source__sector', 'article__source__website').order_by('-article__pub_date')
         context['highest_rated_sources'] = Source.objects.filter(average_rating__gte=4, ammount_of_ratings__gte=1).order_by('average_rating')[:10]
-        context['audio_of_the_week'] = AudioOfTheWeek.objects.all()
+        context['audio_of_the_week'] = AudioOfTheWeek.objects.all().select_related('article__source', 'article__source__sector', 'article__source__website').order_by('-article__pub_date')
         context['newest_sources'] = Source.objects.all()[:10]
         return context
