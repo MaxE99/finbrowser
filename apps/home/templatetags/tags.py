0b79adeac486d27612fe46df231ea1e644192c19
MaxE99 <me-99@live.de>
2022-07-07 18:19:15 +0200

    refactored html completetly with inclusion tags

---
+++ apps/home/templatetags/tags.py
@@ -0,0 +1,28 @@
+# Django import
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('source/source_container.html')
+def create_source_container(title, qs):
+    return {'title': title, 'qs': qs}
+
+@register.inclusion_tag('article/article_container.html')
+def create_article_container(title, qs):
+    return {'title': title, 'qs': qs}
+
+@register.inclusion_tag('article/fk_article_container.html')
+def create_fk_article_container(title, qs):
+    return {'title': title, 'qs': qs}
+
+@register.inclusion_tag('article/tweets_container.html')
+def create_tweets_container(title, qs):
+    return {'title': title, 'qs': qs}
+
+@register.inclusion_tag('article/fk_tweets_container.html')
+def create_fk_tweets_container(title, qs):
+    return {'title': title, 'qs': qs}
+
+@register.inclusion_tag('list/list_slider_container.html')
+def create_list_slider_container(title, qs):
+    return {'title': title, 'qs': qs}
