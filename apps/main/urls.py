1508c642ccf344f0ecfac4a63978a9d1a98e6c8e
MaxE99 <me-99@live.de>
2022-07-10 09:16:25 +0200

    added preload for fonts + refactored cache

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,11 +1,10 @@
 # Django imports
 from django.urls import path
-from django.views.decorators.cache import cache_page
 # Local imports
 from apps.main.views import MainView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', cache_page(3600)(MainView.as_view()), name="main"),
+    path('', MainView.as_view(), name="main"),
 ]
