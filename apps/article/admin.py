81df142d6c0e83b33fb5eb5ee83c59d7dc59572d
MaxE99 <me-99@live.de>
2022-07-17 14:55:08 +0200

    added admin article search

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 # Local imports
 from apps.article.models import Article, HighlightedArticle, AudioOfTheWeek, ArticleOfTheWeek, TrendingTopicArticle, EnergyCrisisTweet, MacroTweets, TweetType
 
+class ArticleSearch(admin.ModelAdmin):
+    search_fields = ['title', ]
+
 admin.site.register(HighlightedArticle)
-admin.site.register(Article)
+admin.site.register(Article, ArticleSearch)
 admin.site.register(TweetType)
 admin.site.register(AudioOfTheWeek)
 admin.site.register(ArticleOfTheWeek)
