58322bdef5f811101d6e655eaddc045e34216924
MaxE99 <me-99@live.de>
2022-07-22 10:19:10 +0200

    fixed minor bugs

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -6,11 +6,15 @@ from apps.article.models import Article, HighlightedArticle, AudioOfTheWeek, Art
 class ArticleSearch(admin.ModelAdmin):
     search_fields = ['title', ]
 
+class SearchInsteadOfDropdown(admin.ModelAdmin):
+    autocomplete_fields = ['article', ]
+
+
 admin.site.register(HighlightedArticle)
 admin.site.register(Article, ArticleSearch)
 admin.site.register(TweetType)
-admin.site.register(AudioOfTheWeek)
-admin.site.register(ArticleOfTheWeek)
-admin.site.register(TrendingTopicArticle)
-admin.site.register(EnergyCrisisTweet)
-admin.site.register(MacroTweets)
+admin.site.register(AudioOfTheWeek, SearchInsteadOfDropdown)
+admin.site.register(ArticleOfTheWeek, SearchInsteadOfDropdown)
+admin.site.register(TrendingTopicArticle, SearchInsteadOfDropdown)
+admin.site.register(EnergyCrisisTweet, SearchInsteadOfDropdown)
+admin.site.register(MacroTweets, SearchInsteadOfDropdown)
