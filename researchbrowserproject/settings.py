e0ddf084c6e1e0fde5c61f1cbc7787fb6cacc061
MaxE99 <me-99@live.de>
2022-09-10 21:46:34 +0200

    changed CSP

--- researchbrowserproject/settings.py
+++ researchbrowserproject/settings.py
@@ -51,11 +51,15 @@ else:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     # CSP Settings
-    CSP_DEFAULT_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/", "https://www.googletagmanager.com", 'sha256-X+zrZv/IbzjZUnhsbWlsecLbwjndTpG0ZynXOif7V+k=')
+    CSP_DEFAULT_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/", "https://www.googletagmanager.com",)
     CSP_STYLE_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/")
-    CSP_SCRIPT_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/", "https://www.googletagmanager.com")
+    CSP_SCRIPT_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/", "https://www.googletagmanager.com",)
     CSP_FONT_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/")
     CSP_IMG_SRC = ("'self'", "https://finbrowser.s3.us-east-2.amazonaws.com/")
+    CSP_INCLUDE_NONCE_IN = [
+        'script-src',
+        'script-src-elem'
+    ]
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
