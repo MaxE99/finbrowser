3f5ebc46fd51a2ae390ef97eb87c735ed61a519c
MaxE99 <me-99@live.de>
2022-09-10 18:11:52 +0200

    design + security refactoring

--- researchbrowserproject/settings.py
+++ researchbrowserproject/settings.py
@@ -50,6 +50,12 @@ else:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+    # CSP Settings
+    CSP_DEFAULT_SRC = ("'self'",)
+    CSP_STYLE_SRC = ("'self'",)
+    CSP_SCRIPT_SRC = ("'self'",)
+    CSP_FONT_SRC = ("'self'",)
+    CSP_IMG_SRC = ("'self'",)
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
