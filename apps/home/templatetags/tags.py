678fa12485e62d3fd6b5d6e2a02b5236686198ba
MaxE99 <me-99@live.de>
2022-12-29 10:42:35 +0100

    small bug fixes

--- apps/home/templatetags/tags.py
+++ apps/home/templatetags/tags.py
@@ -18,3 +18,10 @@ def create_fk_content_container(title, qs, request, user_lists, csrf_token, add_
 @register.inclusion_tag('list/list_slider_container.html')
 def create_list_slider_container(title, qs):
     return {'title': title, 'qs': qs}
+
+@register.filter(name='check_inclusion')
+def check_inclusion(qs1, qs2):
+    for obj in qs1:
+        if(str(obj.name) in qs2):
+            return True
+    return False
