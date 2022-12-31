aa7ae7dfc46b3ce0c9314d80f1e311091181bcac
MaxE99 <me-99@live.de>
2022-12-31 14:35:23 +0100

    added custom timezone support

--- researchbrowserproject/settings.py
+++ researchbrowserproject/settings.py
@@ -130,6 +130,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'researchbrowserproject.middleware.timezone_middleware.TimezoneMiddleware'
 ]
 
 ROOT_URLCONF = 'researchbrowserproject.urls'
