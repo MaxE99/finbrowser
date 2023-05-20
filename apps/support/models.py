cc7a948cfc8eff19f5f2c00de651a9b76555520b
MaxE99 <me-99@live.de>
2023-05-20 08:12:00 +0200

    add search delay

--- apps/support/models.py
+++ apps/support/models.py
@@ -11,6 +11,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=200)
     topic = models.CharField(max_length=200)
     message = models.TextField(max_length=10000)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.email} - {self.topic}"
