eb082b1700f4bbb72ac7ede8037778ad877f1003
MaxE99 <me-99@live.de>
2023-06-24 15:20:33 +0200

    change full-text-search

--- apps/article/managers.py
+++ apps/article/managers.py
@@ -47,10 +47,12 @@ class ArticleManager(models.Manager):
 
     def get_content_about_stock(self, stock):
         q_objects = Q()
-        if len(stock.ticker) > 1 and stock.ticker.lower() not in english_words:
+        if len(stock.ticker) < 3 or stock.ticker.lower() not in english_words:
+            q_objects.add(Q(search_vector=f"${stock.ticker} "), Q.OR)
+        if len(stock.ticker) > 2 and stock.ticker.lower() not in english_words:
             q_objects.add(Q(search_vector=stock.ticker), Q.OR)
-        else:
-            q_objects.add(Q(title__icontains=f"${stock.ticker} "), Q.OR)
+        # else:
+        #     q_objects.add(Q(title__icontains=f"${stock.ticker} "), Q.OR)
         if stock.short_company_name.lower() not in english_words:
             q_objects.add(Q(search_vector=stock.short_company_name), Q.OR)
         return self.filter(q_objects).select_related(
