17559c1ec2a8cd14095f63ba9a13879139f3995b
MaxE99 <me-99@live.de>
2024-10-12 15:33:53 +0200

    enha: add www. name to acm cert

--- aws_infra/modules/certificate/main.tf
+++ aws_infra/modules/certificate/main.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "main" {
-  domain_name       = var.domain
-  validation_method = "DNS"
+  domain_name               = var.domain
+  validation_method         = "DNS"
+  subject_alternative_names = ["www.${var.domain}"]
 
   tags = {
     Project     = var.project
